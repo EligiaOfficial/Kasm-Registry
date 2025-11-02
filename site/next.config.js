dc03888e024d6ea48a593bab5525830e99eaf4ee
Eligia <contact@eligia.net>
2025-11-02 15:02:02 +0100

    Update the basepath

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/EligiaOfficial/Kasm-Registry/issues',
   },
   reactStrictMode: true,
-  basePath: '1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
